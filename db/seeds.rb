851591f9b57a0da498d388eb19a8c94d042dbf58
jackmac92 <jackmac79@gmail.com>
2016-03-30 17:56:36 -0400

    Create seed data with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,15 @@ ActiveRecord::Base.transaction do
   p3 = User.create(email:"guy@gmail.com", password:"password")
   p4 = User.create(email:"friend@gmail.com", password:"password")
   p5 = User.create(email:"pal@gmail.com", password:"password")
+  20.times do
+    User.create email:Faker::Internet.safe_email, password:"password", avatar:Faker::Avatar.image
+  end
 
   Article.destroy_all
   me.articles.create(
     title:"10 Critical Skills You’ll Need to Succeed at Work in 2020",
-    body: <<-BODY
-      Which skills will be most in demand in the coming years? This infographic shows you how to set yourself up for success.
-      Many factors and ingredients work together to create a recipe for success in work and in business. Perhaps, though, one of the most important ingredients to success is the ability to adapt as technology changes and new trends emerge in a fast-paced digital world.
-      A new infographic shows that six key factors are driving the change were seeing right right now: extreme longevity, the rise of smart machines and systems, our computational world, new media ecology, superstructures organizations and the globally connected world.
-      With all of these massive issues in play, what will the working landscape look like in five years? What skills will employees need to succeed?
-      I’ll tell you one thing: they look nothing like the skills desired of workers in the industrial revolution, or even in the dot-com era.
-      In 2020, the ability to come up with solutions, ideas and responses will be highly sought after and propel you to the front of the business line.
-      New media literacy — the ability to critically asses and develop content by utilizing and leveraging new forms of media effectively — will be highly desirable in employees.
-      Cross cultural competency, the ability to operate within different cultural settings, will be needed to thrive in work and business as it becomes increasingly global.
-      A design mindset is also going to be key. If you’re able to represent and develop tasks and work processes that create desired outcomes, you’ll be in demand.
-      And if you can’t work well with others in virtual environments, well… telecommuting isn’t just a trendy thing some hippy employers are offering. Virtual collaboration, the ability to work productively while driving engagement and demonstrating presence as a member of a virtual team, is the way business will increasingly be done.
-      See more of the top ten skills you’ll need to succeed in 2020 (just five short years from now!):
-
+    body: <<-BODY 
+    Which skills will be most in demand in the coming years? This infographic shows you how to set yourself up for success. Many factors and ingredients work together to create a recipe for success in work and in business. Perhaps, though, one of the most important ingredients to success is the ability to adapt as technology changes and new trends emerge in a fast-paced digital world. A new infographic shows that six key factors are driving the change were seeing right right now: extreme longevity, the rise of smart machines and systems, our computational world, new media ecology, superstructures organizations and the globally connected world. With all of these massive issues in play, what will the working landscape look like in five years? What skills will employees need to succeed? I’ll tell you one thing: they look nothing like the skills desired of workers in the industrial revolution, or even in the dot-com era. In 2020, the ability to come up with solutions, ideas and responses will be highly sought after and propel you to the front of the business line. New media literacy — the ability to critically asses and develop content by utilizing and leveraging new forms of media effectively — will be highly desirable in employees. Cross cultural competency, the ability to operate within different cultural settings, will be needed to thrive in work and business as it becomes increasingly global. A design mindset is also going to be key. If you’re able to represent and develop tasks and work processes that create desired outcomes, you’ll be in demand. And if you can’t work well with others in virtual environments, well… telecommuting isn’t just a trendy thing some hippy employers are offering. Virtual collaboration, the ability to work productively while driving engagement and demonstrating presence as a member of a virtual team, is the way business will increasingly be done. See more of the top ten skills you’ll need to succeed in 2020 (just five short years from now!):
     BODY
   )
 
@@ -39,4 +31,8 @@ ActiveRecord::Base.transaction do
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     BODY
   )
+
+  15.times do
+    User.all.sample.articles.create title:Faker::Hacker.say_something_smart, body:Faker::Hipster.paragraphs(4).join(" ")
+  end
 end
