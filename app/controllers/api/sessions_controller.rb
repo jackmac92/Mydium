eaf4421f017f519707522d46747a955b57da65b6
jackmac92 <jackmac79@gmail.com>
2016-03-31 19:02:00 -0400

    Fixing devise

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,13 @@
 class Api::SessionsController < Devise::SessionsController
 	clear_respond_to
   respond_to :json
+  def create  	
+  	super
+  	render json: current_user
+  end
+
+  def destroy
+  	sign_out current_user
+  	render json: {}
+  end
 end
