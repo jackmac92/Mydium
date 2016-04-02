9b85bc7e907537d122ab050187748147fcf11ad4
jackmac92 <jackmac79@gmail.com>
2016-04-01 22:20:59 -0400

    Implement bookmarks favorites and follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
 
   has_many :comments, dependent: :destroy
+  
+  has_many :favorites
+
+  has_many :bookmarks
+
+  has_many :followers, className: "Follows", foreign_key: :followee_id
+
+  has_many :followed_users, className: "Follows", foreign_key: :follower_id
+
 
   def recent_articles_excluding article_id
   	articles.order(:created_at).limit(5).where.not(id: article_id)
