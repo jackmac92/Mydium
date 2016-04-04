469c95313f50f8a4b565fb593e0d8672f1b321c7
jackmac92 <jackmac79@gmail.com>
2016-04-04 08:02:18 -0400

    styling tweaks

--- frontend/components/login_form.jsx
+++ frontend/components/login_form.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ApiUtil = require('../util/api_util');
 
 import Input from 'react-toolbox/lib/input'
+import Header from './mheader'
 import Button from 'react-toolbox/lib/button'
 
 var LoginForm = React.createClass({
@@ -26,14 +27,43 @@ var LoginForm = React.createClass({
 	updatePassword: function (e) {
 		this.setState({password:e})
 	},
+
+	passwordErrors: function () {
+		if (this.state.password.length > 6) {
+			return "";
+		} else {
+			return "Password err"
+		}
+	},
+	emailErrors: function () {
+		if (this.state.email.length > 6) {
+			return "";
+		} else {
+			return "Email err"
+		}
+	},
+
+	formReady: function () {
+		if (this.passwordErrors().length == 0 && this.emailErrors().length == 0) {
+			return true
+		} 
+		return false;
+	},
 	
 	render: function () {
+		var button;
+		if (this.formReady()) {
+			button = <Button raised accent ripple onClick={this.handleSubmit} label="Submit" />
+		} else {
+			button = <Button disabled onClick={this.handleSubmit} label="Submit" />
+		}
 		return (		
 			<div>
-				<h1>YOYOYO you gotta Login</h1>
-				<Input type="email" onChange={this.updateEmail} label="Email" value={this.state.email}/>
-				<Input type="password" onChange={this.updatePassword} value={this.state.password}/>
-				<Button onClick={this.handleSubmit} label="Submit" />
+				<Header />
+				
+				<Input error={this.emailErrors()} type="email" onChange={this.updateEmail} label="Email" value={this.state.email}/>
+				<Input error={this.passwordErrors()} type="password" onChange={this.updatePassword} value={this.state.password}/>
+				{button}
 			</div>
 		)	
 	}
