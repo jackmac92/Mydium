f07eb215375aeca2f6f30682b09d2cfe8267ff42
jackmac92 <jackmac79@gmail.com>
2016-04-23 13:19:36 -0400

    hotfix

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,7 @@ class Follow < Socialization::ActiveRecordStores::Follow
 	include PublicActivity::Common
 	after_save :mark_new_activity
 	# before_destroy :mark_rm_activity
+
 	def mark_new_activity
 		create_activity :follow, recipient: followable_type.constantize.find(followable_id), owner:follower_type.constantize.find(follower_id)
 	end
