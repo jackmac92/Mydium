85db50fdfb0182060c8b5d5847b4ab1b1139da2f
jackmac92 <jackmac79@gmail.com>
2016-05-05 16:10:49 -0400

    fixing userinfo error

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,16 @@ Rails.application.routes.draw do
   namespace :api, defaults:{format: :json} do
     resources :articles, only:[:index, :create, :update, :show, :destroy]
     resource :auth, only:[:show]
-    resource :user, only:[:update]
-    resources :users, only:[:show]
+    # resource :user, only:[:update]
+    # resources :users, only:[:show]
     resources :comments, only:[:create, :destroy]
     resources :tags, only:[:index]
     resources :searches, only:[:index]
 
     resources :article_reads, only:[:create]
 
+    get "/users/:id", to: 'users#show'
+    patch "/user/:id", to: 'users#update'
     patch '/articles/:id/publish', to: 'articles#publish'
     patch '/articles/:id/unpublish', to: 'articles#unpublish'
     patch '/articles/:id/autosave', to: 'articles#autosave'
@@ -32,6 +34,5 @@ Rails.application.routes.draw do
     get '/user/drafts', to: 'users#drafts' 
     get '/user/published', to: 'users#published' 
     
-    get "*unmatched_route", to: 'static_pages#root'
   end
 end
